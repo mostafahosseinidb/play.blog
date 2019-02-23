906c1db26eeb48e3a5c99f6edca8c91a3ed6a597
mostafa hosseini <mostafa.hosseini.db@gmail.com>
2019-02-23 11:23:26 +0330

    Updates

---
+++ precache-manifest.a42a800c5d96db4d3ab86e37a43189ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/play_pod_blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "666cbf422f53cdb88280",
+    "url": "/play_pod_blog/static/js/runtime~main.666cbf42.js"
+  },
+  {
+    "revision": "9da0de6626360603c99c",
+    "url": "/play_pod_blog/static/js/main.9da0de66.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/play_pod_blog/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "9da0de6626360603c99c",
+    "url": "/play_pod_blog/static/css/main.b384c933.chunk.css"
+  },
+  {
+    "revision": "cb8614d35d6a69a8aab3572eb41729e7",
+    "url": "/play_pod_blog/index.html"
+  }
+];
